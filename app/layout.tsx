f14e65fad7c60b33043fac373abe76da84071213
KAWAKAMI Moeki <hi@moeki.org>
2025-12-08 22:20:18 +0900

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
+          content="width=device-width,initial-scale=1.0,maximum-scale=1.0"
         />
       </head>
       <body
